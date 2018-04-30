993bff5881fea1ad4f2aadd29bd39fe898377f5a
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-04-30 09:11:52 -0700

    Reading_CVSs_checkpoint_6_completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -85,7 +85,7 @@ RSpec.describe AddressBook do
       end
 
       it "imports the 3rd entry" do
-        book.import_from_csv("entries.cvs")
+        book.import_from_csv("entries.csv")
         entry_three = book.entries[2]
 
         check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
