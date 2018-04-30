19887f2b7aadf69bf2234650937f5b331f35c84e
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-04-30 14:25:00 -0700

    Searching_checkpoint_7_completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -47,7 +47,24 @@ class AddressBook
   end
 
   def binary_search(name)
-    #search AddressBook for a specific entry by name 
+    #search AddressBook for a specific entry by name
+    lower = 0
+    upper = entries.length - 1
+
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      if name ==mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    return nil 
   end
 
 
