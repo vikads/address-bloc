421e56b048fe31926398417d942c008aaa2ed4b7
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-01 10:41:14 -0700

    Import

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -107,6 +107,24 @@ require_relative '../models/address_book'
   end
 
   def read_csv
+
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin
+      entry_count = address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end 
   end
 
   def entry_submenu(entry)
