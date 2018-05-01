cf3ee4e2c9626f4b8c8ab24a388436f715e486d3
Viktoria Dounar-Schabowska <viktoria.dounar@gmail.com>
2018-05-01 11:02:43 -0700

    Edit entries

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -111,6 +111,24 @@ require_relative '../models/address_book'
     puts "#{entry.name} has been deleted"
   end
 
+  def edit_entry(entry)
+
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number: "
+    phone_number = gets.chomp
+    print = "Updated email: "
+    email = gets.chomp
+
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+
+    puts "Updated entry:"
+    puts entry 
+  end
+
   def read_csv
 
     print "Enter CSV file to import: "
